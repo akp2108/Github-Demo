347937b4d5fc7c8a442640827adecae0d184beb0
akp2108@gmail.com <Brisbane@123>
2015-08-26 13:26:16 +1000

    Changed text in output message

--- src/com/demo/github/MathFunctions.java
+++ src/com/demo/github/MathFunctions.java
@@ -2,18 +2,18 @@ package com.demo.github;
 
 public class MathFunctions {
 	
-	int a, b, c;
+	//int a, b, c;
 	
 	public void Addition(int a, int b){
 		int c;		
 		c = a + b;		
-		System.out.println(c);
+		System.out.println("Addition of two numbers is : " + c);
 	}
 	
 	public void Subtraction(int a, int b){
 		int c;		
 		c = a - b;		
-		System.out.println(c);
+		System.out.println("Subtraction of two numbers is : " + c);
 	}
 
 }
