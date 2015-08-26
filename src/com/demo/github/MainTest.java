1da8950fc0d6ced615ed29f3f9e6fe3c90ebad38
akp2108@gmail.com <Brisbane@123>
2015-08-26 13:09:01 +1000

    Initial checkin

---
+++ src/com/demo/github/MainTest.java
@@ -0,0 +1,13 @@
+package com.demo.github;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+
+		MathFunctions functions = new MathFunctions();
+		functions.Addition(10, 20);
+		functions.Subtraction(20, 10);
+		
+	}
+
+}
---
+++ src/com/demo/github/MathFunctions.java
@@ -0,0 +1,19 @@
+package com.demo.github;
+
+public class MathFunctions {
+	
+	int a, b, c;
+	
+	public void Addition(int a, int b){
+		int c;		
+		c = a + b;		
+		System.out.println(c);
+	}
+	
+	public void Subtraction(int a, int b){
+		int c;		
+		c = a - b;		
+		System.out.println(c);
+	}
+
+}
